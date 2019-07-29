24ad6140ec51744e41fc0f0c75e5d925e9f169df
Matt Weiss <weiss.matt@outlook.com>
2019-07-29 11:22:31 -0600

    Creates tests for model relaitonships and validations

--- spec/models/event_spec.rb
+++ spec/models/event_spec.rb
@@ -1,11 +1,11 @@
 require 'rails_helper'
 
-describe Olympian, type: :model do
+describe Event, type: :model do
   describe 'validations' do
-    it{should validate_presence_of(:name)}
+    it{should validate_presence_of(:games)}
   end
   describe 'relationships' do
     it{should have_many :olympian_events}
-    it{should have_many :events}
+    it{should have_many :olympians}
   end
 end
