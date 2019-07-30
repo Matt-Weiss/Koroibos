d8d71923be4c2be44aa7cc687cbde285d0be4776
Matt Weiss <weiss.matt@outlook.com>
2019-07-29 21:27:54 -0600

    Adds search params for age queries

--- app/controllers/api/v1/olympians_controller.rb
+++ app/controllers/api/v1/olympians_controller.rb
@@ -1,7 +1,13 @@
 class Api::V1::OlympiansController < ApplicationController
 
   def index
-    render json: OlympianSerializer.parse_olympians
+    render json: OlympianSerializer.parse_olympians(search_params)
+  end
+
+  private
+
+  def search_params
+    params.permit(:age)
   end
 
 end
