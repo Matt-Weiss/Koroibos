e8a024889b3cd3401adb23f9b1b45d7e9d0bcac6
Matt Weiss <weiss.matt@outlook.com>
2019-07-29 20:19:32 -0600

    Moves setup into a before each

--- spec/requests/api/v1/olympians/olympian_spec.rb
+++ spec/requests/api/v1/olympians/olympian_spec.rb
@@ -1,42 +1,41 @@
 require 'rails_helper'
 
 describe 'Olympians endpoint' do
+  before :each do
+    olympian_1 = Olympian.create(name: "Maha Abdalsalam Gouda",
+                                  sex: "F",
+                                  age: 18,
+                               height: 172,
+                               weight: 61,
+                                 team: "Egypt")
+    event_1 = Event.create(games: "2016 Summer",
+                           sport: "Diving",
+                           event: "Diving Women's Platform")
+    OlympianEvent.create(olympian_id: olympian_1.id,
+                            event_id: event_1.id,
+                               medal: "NA")
+
+    olympian_2 = Olympian.create(name: "Ahmad Abughaush",
+                                  sex: "M",
+                                  age: 20,
+                               height: 178,
+                               weight: 68,
+                                 team: "Jordan")
+    event_2 = Event.create(games: "2016 Summer",
+                           sport: "Taekwondo",
+                           event: "Taekwondo Men's Featherweight")
+    OlympianEvent.create(olympian_id: olympian_2.id,
+                            event_id: event_2.id,
+                               medal: "Gold")
+  end
   it 'can hit endpoint successfully' do
     get '/api/v1/olympians'
     expect(response.status).to eq(200)
   end
 
   it 'retrieves correct olympian data' do
-    olympian_1 = Olympian.create(name: "Maha Abdalsalam Gouda",
-                     sex: "F",
-                     age: 18,
-                  height: 172,
-                  weight: 61,
-                    team: "Egypt")
-    event_1 = Event.create(games: "2016 Summer",
-                 sport: "Diving",
-                 event: "Diving Women's Platform")
-    OlympianEvent.create(olympian_id: olympian_1.id,
-                            event_id: event_1.id,
-                               medal: "NA")
-
-    olympian_2 = Olympian.create(name: "Ahmad Abughaush",
-                     sex: "M",
-                     age: 20,
-                  height: 178,
-                  weight: 68,
-                    team: "Jordan")
-    event_2 = Event.create(games: "2016 Summer",
-                 sport: "Taekwondo",
-                 event: "Taekwondo Men's Featherweight")
-    OlympianEvent.create(olympian_id: olympian_2.id,
-                            event_id: event_2.id,
-                               medal: "Gold")
-
-
     get '/api/v1/olympians'
     data = JSON.parse(response.body)
-    # binding.pry
     expect(data["olympians"][0]["team"]).to eq("Egypt")
   end
 end
